102cd884d07fe2e3ae641868e3be1c5debdbaa4d
AsenaAyaz <asenaayaz0@gmail.com>
2022-07-15 17:25:45 +0300

    Add project files.

---
+++ denemelerim/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace denemelerim
+{
+    class MusteriManager
+    {
+        public void musteriEkle (Musteri name)
+        {
+            Console.WriteLine(name.MusteriAdi+"    eklendi");
+            Console.WriteLine("------------------");
+        }
+
+        public void musteriSil (Musteri surname)
+        {
+            Console.WriteLine(surname.MusteriSoyadi + " silindi");
+            Console.WriteLine("-------------------");
+
+        }
+
+        public void musteriListele( Musteri puan)
+        {
+            Console.WriteLine(puan.MusteriPuani +" listelendi");
+            Console.WriteLine("--------------------");
+        }
+    }
+}
---
+++ denemelerim/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace denemelerim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.MusteriAdi = "Asena";
+            musteri1.MusteriSoyadi = "Ayaz";
+            musteri1.MusteriPuani = 10;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.musteriEkle(musteri1);
+            musteriManager.musteriSil(musteri1);
+            musteriManager.musteriSil(musteri1);
+        }
+    }
+}
